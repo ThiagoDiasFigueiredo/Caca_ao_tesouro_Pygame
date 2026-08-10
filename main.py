d553e240f51b198c96e962db2b78d23ec2b9ec26
Matheus Meira <mthsbks@gmail.com>
2026-08-10 15:51:53 -0300

    atualizei as imagens

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import random
 def main():
     # Tela
     pygame.init()
-    largura = 200
-    altura = 250
+    largura = 500
+    altura = 600
     tela = pygame.display.set_mode((largura, altura))
     pygame.display.set_caption('Caça ao tesouro')
     estado = 'menu'
@@ -13,24 +13,30 @@ def main():
     som_tesouro = pygame.mixer.Sound("pygame_tesouro.wav")
     som_buraco = pygame.mixer.Sound("pygame_buraco.wav")
     som_contagem = pygame.mixer.Sound("pygame_contagem.wav")
-    img_tesouro = pygame.transform.scale(pygame.image.load("tesouro.png"), (49, 49))
-    img_buraco = pygame.transform.scale(pygame.image.load("buraco.png"), (49, 49))
-    img_menu = pygame.transform.scale(pygame.image.load('menu.png'),(200,250))
+
+    img_tesouro = pygame.transform.scale(pygame.image.load("tesouro.png"), (46, 46))
+    img_buraco = pygame.transform.scale(pygame.image.load("buraco1.png"), (46, 46))
+    img_menu = pygame.transform.scale(pygame.image.load('menu.png'),(500,650))
+    img_vitoria = pygame.transform.scale(pygame.image.load('vitoria.png'),(500,600))
+    img_empate = pygame.transform.scale(pygame.image.load('empate.png'),(500,600))
+
     cores = {
         'branca': (255, 255, 255),
         'azul': (0, 0, 255),
         'vermelho': (255, 0, 0),
         'preto': (0, 0, 0),
-        'verde': (0, 255, 0),
+        'marrom': (101,67,33),
+        'areia': (194,178,128),
     }
 
-    fonte = pygame.font.Font(None, 20)
+    fonte1 = pygame.font.Font(None, 40)
+    fonte2 = pygame.font.Font(None, 30)
     lado_quadrado = 50
-    num_linhas = 4
-    tamanho_grade = lado_quadrado * num_linhas  # 200
+    num_linhas = 11
+    tamanho_grade = lado_quadrado * (num_linhas - 1)  # 200
 
-    linhas = 4
-    colunas = 4
+    linhas = 10
+    colunas = 10
 
     # Looping do jogo
     running = True
@@ -38,21 +44,25 @@ def main():
         for evento in pygame.event.get():
             if evento.type == pygame.QUIT:
                 running = False
-
+            
             if evento.type == pygame.KEYDOWN:
+                if evento.key == pygame.K_ESCAPE and estado == 'menu':
+                    running = False
+                    print('fechou')
+
                 if evento.key == pygame.K_RETURN and estado == 'menu':
                     pontuacao = [0,0]
 
-                    tela.fill(cores['branca'])
+                    tela.fill(cores['areia'])
 
                     pos_y = 0
                     for i in range(num_linhas):
-                        pygame.draw.line(tela, cores['preto'], (0, pos_y), (tamanho_grade, pos_y),1)
+                        pygame.draw.line(tela, cores['marrom'], (0, pos_y), (tamanho_grade, pos_y),3)
                         pos_y += lado_quadrado
 
                     pos_x = 0
                     for j in range(num_linhas):
-                        pygame.draw.line(tela, cores['preto'], (pos_x, 0), (pos_x, tamanho_grade), 1)
+                        pygame.draw.line(tela, cores['marrom'], (pos_x, 0), (pos_x, tamanho_grade), 3)
                         pos_x += lado_quadrado
 
                     tabuleiro = []
@@ -62,8 +72,8 @@ def main():
                             linha.append(0)
                         tabuleiro.append(linha)
 
-                    tesouros = 6
-                    buracos = 3
+                    tesouros = 12
+                    buracos = 8
                     contador = 0
                     while contador < tesouros:
                         linha = random.randint(0, linhas - 1)
@@ -82,7 +92,7 @@ def main():
 
                     for linha_tab in tabuleiro:
                         print(linha_tab)
-
+                    
                     revelado = []
                     for i in range(linhas):
                         revelado.append([0] * colunas)
@@ -103,6 +113,19 @@ def main():
                                 if j + 1 < colunas:
                                     if tabuleiro[i][j + 1] == -1:
                                         contar_tesouros += 1
+                                if i - 1 >= 0 and j - 1 >= 0:
+                                    if tabuleiro[i - 1][j - 1] == -1:
+                                        contar_tesouros += 1
+                                if i - 1 >= 0 and j + 1 < colunas:
+                                    if tabuleiro[i - 1][j + 1] == -1:
+                                        contar_tesouros += 1
+                                if i + 1 < linhas and j - 1 >= 0:
+                                    if tabuleiro [i + 1][j - 1] == -1:
+                                        contar_tesouros += 1
+                                if i + 1 < linhas and j + 1 < colunas:
+                                    if tabuleiro [i + 1][j + 1] == -1:
+                                        contar_tesouros += 1
+
                                 tabuleiro[i][j] = contar_tesouros
 
                     vez = random.randint(1,2)
@@ -122,7 +145,7 @@ def main():
                 x, y = evento.pos
                 coluna = x // 50
                 linha = y // 50
-                if y >= 200:
+                if y >= 500:
                     continue
                 if revelado[linha][coluna] != 0:
                     continue
@@ -164,51 +187,55 @@ def main():
 
         # RENDERIZAÇÃO
         if estado == 'vitoria':
-            tela.fill(cores['branca'])
             if vencedor == 'Jogador 1':
-                texto_vitoria1 = fonte.render('O vencedor da partida foi:', False, cores['preto'])
-                texto_vitoria2 = fonte.render(f'{vencedor}', False, cores['azul'])
-                tela.blit(texto_vitoria1, (20, 30)) 
-                tela.blit(texto_vitoria2, (65, 50))
-
+                texto_vitoria1 = fonte1.render('O vencedor da partida foi:', False, cores['branca'])
+                texto_vitoria2 = fonte1.render(f'{vencedor}', False, cores['azul'])
+                tela.blit(img_vitoria,(0,0))
+                tela.blit(texto_vitoria1, (60, 20)) 
+                tela.blit(texto_vitoria2, (170, 150))
+                
             elif vencedor == 'Jogador 2':
-                texto_vitoria1 = fonte.render('O vencedor da partida foi:', False, cores['preto'])
-                texto_vitoria2 = fonte.render(f'{vencedor}', False, cores['vermelho'])
-                tela.blit(texto_vitoria1, (20, 30)) 
-                tela.blit(texto_vitoria2, (65, 50))
-
+                texto_vitoria1 = fonte1.render('O vencedor da partida foi:', False, cores['branca'])
+                texto_vitoria2 = fonte1.render(f'{vencedor}', False, cores['vermelho'])
+                tela.blit(img_vitoria,(0,0))
+                tela.blit(texto_vitoria1, (60, 20)) 
+                tela.blit(texto_vitoria2, (170, 150))
+                
             elif vencedor == 'Empate':
-                texto_vitoria1 = fonte.render('Houve um EMPATE', False, cores['preto'])
-                tela.blit(texto_vitoria1, (42, 50))
-
-            texto_retorno = fonte.render('ENTER para retornar ao menu', False, cores['preto'])
-            
-            tela.blit(texto_retorno, (5, 150))
-            texto_fecha = fonte.render('Aperte ESC para sair do jogo', False, cores['preto'])
-            tela.blit(texto_fecha, (10, 210))
+                texto_vitoria1 = fonte1.render('Houve um EMPATE', False, cores['branca'])
+                tela.blit(img_empate,(0,0))
+                tela.blit(texto_vitoria1, (42, 20))
+                
+            texto_retorno = fonte1.render('ENTER para retornar ao menu', False, cores['branca'])
+            tela.blit(texto_retorno, (60, 445))
+            texto_fecha = fonte1.render('Aperte ESC para sair do jogo', False, cores['branca'])
+            tela.blit(texto_fecha, (60, 485))
 
         if estado == 'menu':
             tela.fill(cores['preto'])
-            texto_menu = fonte.render('Aperte Enter para jogar', False, cores['branca'])
+            texto_menu1 = fonte1.render('Aperte ENTER para jogar', False, cores['branca'])
+            texto_menu2 = fonte1.render('Aperte ESC para sair', False, cores['branca'])
             tela.blit(img_menu, (0,0))
-            tela.blit(texto_menu,(20,170))
+            tela.blit(texto_menu1,(70,325))
+            tela.blit(texto_menu2,(95,375))
+
         if estado == 'jogando':
-            pygame.draw.rect(tela, cores['branca'], (0, 200, 200, 50))
-            texto_jogador_1 = fonte.render(f'Jogador 1 - Pontuação:{pontuacao[0]}', False, cores['azul'])
-            texto_jogador_2 = fonte.render(f'Jogador 2 - Pontuação:{pontuacao[1]}', False, cores['vermelho'])
-            texto_vez = fonte.render(f'Vez:{vez}', False, cores['preto'])
-            tela.blit(texto_vez, (20, 201))
-            tela.blit(texto_jogador_1, (20, 210))
-            tela.blit(texto_jogador_2, (20, 230))
+            pygame.draw.rect(tela, cores['areia'], (0, 505, largura, altura - 505))
+            texto_jogador_1 = fonte2.render(f'Jogador 1 - Pontuação:{pontuacao[0]}', False, cores['azul'])
+            texto_jogador_2 = fonte2.render(f'Jogador 2 - Pontuação:{pontuacao[1]}', False, cores['vermelho'])
+            texto_vez = fonte2.render(f'Vez:{vez}', False, cores['preto'])
+            tela.blit(texto_vez, (20, 510))
+            tela.blit(texto_jogador_1, (20, 540))
+            tela.blit(texto_jogador_2, (20, 570))
 
             for i in range(linhas):
                 for j in range(colunas):
                     if revelado[i][j] == 1:
-                        tela.blit(img_tesouro, (j * 50 + 1, i * 50 + 1))
+                        tela.blit(img_tesouro, (j * 50 + 2, i * 50 + 2))
                     elif revelado[i][j] == 2:
-                        tela.blit(img_buraco, (j * 50 + 1, i * 50 + 1))
+                        tela.blit(img_buraco, (j * 50 + 2, i * 50 + 2))
                     elif revelado[i][j] == 3:
-                        texto_numeros = fonte.render(str(tabuleiro[i][j]), True, cores['preto'])
+                        texto_numeros = fonte1.render(str(tabuleiro[i][j]), True, cores['preto'])
                         tela.blit(texto_numeros, (j * 50 + 20, i * 50 + 20))
 
             acabou = True
@@ -216,7 +243,7 @@ def main():
                 for j in range(colunas):
                     if revelado[i][j] == 0:
                         acabou = False
-
+                    
             if acabou:
                 if pontuacao[0] > pontuacao[1]:
                     vencedor = 'Jogador 1'
